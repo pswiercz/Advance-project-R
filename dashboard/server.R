ac1f41c896b5ec5c8cd02addf109f3d6408eed48
Alicja  Siewarga <alicjasiewarga@macbook-pro-5.home>
2020-05-27 17:07:25 +0200

    add mock plots

--- dashboard/server.R
+++ dashboard/server.R
@@ -1,7 +1,36 @@
-
+library(tidyverse)
 library(shiny)
 
+#Here is place for data inputing
+data <- iris
+#I use iris as mock data
+
+
 shinyServer(function(input, output) {
+    output$plt <- renderPlot({
+        if (input$rb == '2012') {
+            ggplot(data) + aes_(iris$Sepal.Length, iris$Sepal.Width) + geom_point()
+        }
+        else if(input$rb == '2013'){
+            ggplot(data) + aes_(iris$Petal.Length) + geom_bar()
+        }
+        else if(input$rb == '2014'){
+            ggplot(data) + aes_(iris$Petal.Width) + geom_histogram()
+        }
+        else{
+            NULL
+        }
+    })
+    
+    output$plt2 <- renderPlot({
+        if(input$checkBx == 'Name1'){
+            ggplot(data) + aes_(iris$Sepal.Length) + geom_bar()
+        }
+        #...
+        else{
+            NULL
+        }
+    })
 
 
 })
--- dashboard/ui.R
+++ dashboard/ui.R
@@ -3,6 +3,28 @@ library(shiny)
 shinyUI(fluidPage(
 
     titlePanel("Dashboard App"),
-
+    sidebarLayout(
+        sidebarPanel(
+            radioButtons(
+                inputId = "rb",
+                label = "Choose year",
+                choiceNames = list('2012', '2013', '2014'),
+                choiceValues = list('2012', '2013', '2014')
+            )
+        ),
+        mainPanel(plotOutput("plt"))
+    ),
+    sidebarLayout(
+        sidebarPanel(
+            checkboxGroupInput(
+                inputId ='checkBx',
+                label = "District Manager",
+                choiceNames = (list("Name1", "Name2", 'Name3')),
+                choiceValues = (list("Name1", "Name2", 'Name3'))
+            )
+        ),
+        mainPanel(plotOutput("plt2"))
     )
 )
+)
+
